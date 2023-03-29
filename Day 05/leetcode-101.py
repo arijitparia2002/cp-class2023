edd392380914cd2bc17d65a317e51866c257cc4e
Arijit Paria <arijitparia2020@gmail.com>
2023-03-29 13:29:20 +0530

    Added leetcode 101

---
+++ Day 05/leetcode-101.py
@@ -0,0 +1,34 @@
+# 101. Symmetric Tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        if root is None:
+            return True
+
+
+        def dfs(left, right):
+            if left == None and right == None:
+                return True
+
+            if left == None or right == None:
+                return False
+
+            if left.val != right.val:
+                return False
+
+            return dfs(left.right, right.left) and dfs(left.left, right.right)
+        
+        return dfs(root.left, root.right)
+        
+
+            

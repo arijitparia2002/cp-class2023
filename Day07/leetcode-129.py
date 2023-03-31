6923da075cab921e8b6ac5d092a30c8a031536dc
Arijit Paria <arijitparia2020@gmail.com>
2023-03-31 12:12:47 +0530

    another approch added

--- Day07/leetcode-129.py
+++ Day07/leetcode-129.py
@@ -26,6 +26,43 @@ class Solution(object):
         self.ans = 0
         DFS(root, 0)
         return self.ans
+    
+########################################################################################################
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        def DFS(root, currVal):
+            if root == None:
+                return
+            DFS(root.right, currVal*10 + root.val)
+            
+            if root.right == None and root.left==None:
+                self.ans += (currVal*10 + root.val)
+
+            DFS(root.left, currVal*10 + root.val)
+
+        self.ans = 0
+        DFS(root, 0)
+        return self.ans
+            
+            
+
+
+            
+
             
             
 
